60276d14d4ea61e9a784034d0ee158172a98f563
Elwyn Cruz <elwyncruz@gmail.com>
2025-12-05 23:40:19 -0800

    attempts another fix at js build errors

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/elwyncruz.github.io" : "",
-  assetPrefix: isProd ? "/elwyncruz.github.io/" : "",
   trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 module.exports = nextConfig;
